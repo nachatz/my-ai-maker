4afae307308c3c74362c8b85c0590f3eb6fb73c6
nachatz <nik.achatz@gmail.com>
2023-07-04 01:09:19 -0700

    refactor: using chi instead of gorilla

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 	}
 
 	// Initialize the router
-	router := handlers.InitializeRoutes()
+	router := handlers.InitRoutes()
 
 	// Start the server on port 8080
 	log.Fatal(http.ListenAndServe(":8080", router))
--- app/internal/handlers/endpoints.go
+++ app/internal/handlers/endpoints.go
@@ -6,54 +6,60 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi"
 	"github.com/nachatz/my-ai-maker/app/internal/api"
 	"github.com/nachatz/my-ai-maker/app/internal/middleware"
 	"github.com/nachatz/my-ai-maker/app/internal/models"
 )
 
-func InitializeRoutes() *mux.Router {
-	/* InitializeRoutes - Initializes the API routes and returns the router.
-	   @Return *mux.Router - The initialized router.
-	*/
-	var router = mux.NewRouter()
+func InitRoutes() http.Handler {
+	mux := chi.NewRouter()
 
-	// load in environment variables from .env file using os package
+	// load environment variables
 	clientSecret := os.Getenv("CLIENT_SECRET")
-	clientId := os.Getenv("CLIENT_ID")
-	if clientSecret == "" || clientId == "" {
-		log.Fatal("CLIENT_SECRET environment variable not set")
+	clientID := os.Getenv("CLIENT_ID")
+	if clientSecret == "" || clientID == "" {
+		log.Fatal("CLIENT_SECRET or CLIENT_ID environment variable not set")
 		return nil
 	}
 
-	// Routes
-	router.Handle(api.EndpointProcess, middleware.JwtMiddleware(http.HandlerFunc(ProcessHandler), clientSecret)).Methods("POST")
-	router.HandleFunc(api.EndpointAuthToken, func(w http.ResponseWriter, r *http.Request) { GenerateJWTHandler(w, r, clientSecret, clientId) }).Methods("POST")
-	router.MethodNotAllowedHandler = methodNotAllowedHandler()
+	// Non-authenticated endpoints
+	mux.Post(api.EndpointAuthToken, func(w http.ResponseWriter, r *http.Request) {
+		GenerateJWTHandler(w, r, clientSecret, clientID)
+	})
+
+	// Authenticated endpoints
+	mux.Group(func(r chi.Router) {
+		r.Use(func(next http.Handler) http.Handler {
+			return middleware.JwtMiddleware(next, clientSecret)
+		})
+		r.Post(api.EndpointProcess, ProcessHandler)
+	})
+
+	mux.MethodNotAllowed(methodNotAllowedHandler())
 
 	// log the names of all the endpoints in the router
-	logEndpointNames(router)
+	logEndpointNames(mux)
 
-	return router
-
+	return mux
 }
 
-func logEndpointNames(router *mux.Router) {
+func logEndpointNames(router chi.Router) {
 	/* logEndpointNames - Logs the names of all the endpoints in the router.
-	   @Param *mux.Router - The router to log the endpoint names of.
+	   @Param chi.Router - The router to log the endpoint names of.
 	*/
-	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		path, err := route.GetPathTemplate()
-		if err != nil {
-			log.Fatal("Error getting path template")
-			return err
-		}
-		log.Println("Endpoint:", path)
+	walker := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Println("Endpoint:", route)
 		return nil
-	})
+	}
+
+	if err := chi.Walk(router, walker); err != nil {
+		log.Fatal("Error walking the router:", err)
+	}
 }
 
-func methodNotAllowedHandler() http.Handler {
+
+func methodNotAllowedHandler() http.HandlerFunc {
 	/* methodNotAllowedHandler - Handles the method not allowed error by returning an appropriate response.
 	   @Return http.Handler - The handler for method not allowed error.
 	*/

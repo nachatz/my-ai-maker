192ddcdb603538bfec27bb67ce926f7b26f2ad24
nachatz <nik.achatz@gmail.com>
2023-10-16 01:01:34 -0700

    feat: env builder

---
+++ client/create-env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Specify the .env file path
+env_file=".env"
+
+# Define the content for the .env file
+env_content=$(cat <<EOF
+DATABASE_URL: nosir,
+NEXTAUTH_SECRET: nosir,
+GOOGLE_CLIENT_ID: nosir,
+GOOGLE_CLIENT_SECRET: nosir
+EOF
+)
+
+# Write the content to the .env file
+echo "$env_content" > "$env_file"
+
+echo "Created fake $env_file"

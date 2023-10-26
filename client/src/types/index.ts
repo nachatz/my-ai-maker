b5328804c4da9b6f7d807158cc46060f70d5b7e1
nachatz <nik.achatz@gmail.com>
2023-10-25 22:10:49 -0700

    fix: properely exporting index types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,5 @@
+export type { CardType } from "./components/Card";
+export type { CardArray } from "./components/Card";
 export type { Props as FeaturesProps } from "./components/Features";
 export type { Props as NavItemsProps } from "./components/NavItem";
 export type { Props as PricingProps } from "./components/Pricing";

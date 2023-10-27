50077a6e52d43cdd2f22e1bbc9c8d13b8e87a47e
nachatz <nik.achatz@gmail.com>
2023-10-26 23:02:51 -0700

    feat: cleaner login page

--- client/src/pages/auth/login.tsx
+++ client/src/pages/auth/login.tsx
@@ -5,61 +5,75 @@ import type {
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "~/server/auth";
-import { useRouter } from "next/router";
 
 export default function login({
   providers,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
   return (
-    <>
-      <div className="mt-[6rem] flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-        <div className="mt-24 flex justify-center">
-          {Object.values(providers).map((provider) => (
-            <button
-              className="w-25 flex rounded-md bg-indigo-600 px-5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              key={provider.id}
-              onClick={() =>
-                void signIn(provider.id, {
-                  callbackUrl: `${window.location.origin}`,
-                })
-              }
-            >
-              Sign in with Google
-            </button>
-          ))}
-          {/* eslint-disable-next-line  @next/next/no-html-link-for-pages*/}
-          <a
-            // Since the navbar is rendered on client, needs a hard refresh
-            onClick={() => {
-              router.reload();
-            }}
-            href="/"
-            className="w-25 ml-5 flex rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Back
-          </a>
-        </div>
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <p className="mt-5 text-center text-sm text-gray-500">
-            Don&apos;t have a Google account?{" "}
-            <a
-              target="_blank"
-              href="https://www.google.com/account/about/"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Create one for free
-            </a>
-          </p>
+    <main className="flex h-[75vh] items-center justify-center">
+      <div className="w-full max-w-md p-6">
+        <div className="mt-7 rounded-xl border border-gray-200 bg-white shadow-2xl">
+          <div className="p-4 sm:p-7">
+            <div className="text-center">
+              <h1 className="block text-2xl font-bold text-gray-800">
+                Sign in
+              </h1>
+              <p className="mt-2 text-sm text-gray-600">
+                Don&apos;t have an account yet?
+                <a
+                  className="ml-2 font-medium text-blue-600 decoration-1 hover:underline"
+                  target="_blank"
+                  href="https://www.google.com/account/about/"
+                >
+                  Sign up here
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-5">
+              {Object.values(providers).map((provider) => (
+                <button
+                  key={provider.id}
+                  onClick={() =>
+                    void signIn(provider.id, {
+                      callbackUrl: `${window.location.origin}`,
+                    })
+                  }
+                  type="button"
+                  className=":bg-slate-800 :text-white :ring-offset-gray-800 inline-flex w-full items-center justify-center gap-2 rounded-md border bg-white px-4 py-3 align-middle text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-50 focus:outline-none focus:ring-2  focus:ring-blue-600   focus:ring-offset-2 focus:ring-offset-white"
+                >
+                  <svg
+                    className="h-auto w-4"
+                    width="46"
+                    height="47"
+                    viewBox="0 0 46 47"
+                    fill="none"
+                  >
+                    <path
+                      d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
+                      fill="#4285F4"
+                    />
+                    <path
+                      d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
+                      fill="#34A853"
+                    />
+                    <path
+                      d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
+                      fill="#FBBC05"
+                    />
+                    <path
+                      d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
+                      fill="#EB4335"
+                    />
+                  </svg>
+                  Sign in with Google
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
